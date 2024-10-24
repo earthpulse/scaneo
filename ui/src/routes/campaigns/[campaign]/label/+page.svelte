6530e09b639ff2722451aad2ae5bd46638d8c3af
Juan Sensio <sensio.juan@gmail.com>
2024-10-24 17:13:21 +0200

    add map

--- ui/src/routes/campaigns/[campaign]/label/+page.svelte
+++ ui/src/routes/campaigns/[campaign]/label/+page.svelte
@@ -1,15 +1,18 @@
 <script>
   import { page } from "$app/stores";
-
   import TopBar from "./TopBar.svelte";
   import LeftBar from "./LeftBar.svelte";
   import Map from "./Map.svelte";
   import RightBar from "./RightBar.svelte";
   import images from "$stores/images.svelte.js";
+  import TileLayer from "./TileLayer.svelte";
+  import LayersControl from "./LayersControl.svelte";
 
   $effect(() => {
     images.retrieve($page.params.campaign);
   });
+
+  let layer = $state("streets");
 </script>
 
 <div class="h-full flex-1 flex flex-col">
@@ -17,7 +20,24 @@
   <div class="flex flex-row w-full h-full flex-1">
     <LeftBar />
     <div class="flex-1">
-      <Map />
+      <Map position={[42.0407, 3.1]} zoom={4}>
+        <TileLayer
+          url={"https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"}
+          options={{ maxZoom: 20, zIndex: 1 }}
+        />
+        {#if layer == "streets"}
+          <TileLayer
+            url={"https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"}
+            options={{ maxZoom: 20, zIndex: 1 }}
+          />
+        {:else if layer == "satellite"}
+          <TileLayer
+            url={"https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"}
+            options={{ maxZoom: 20, zIndex: 1 }}
+          />
+        {/if}
+        <LayersControl layers={["streets", "satellite"]} bind:layer />
+      </Map>
     </div>
     <RightBar />
   </div>
