b283ed8bf326ae8333cfd4ddf893822097c5a0be
Fernando Hernandez <ferndohm99emf@gmail.com>
2024-11-04 22:51:21 +0100

    added color picker on labels

--- ui/src/lib/labels/create.js
+++ ui/src/lib/labels/create.js
@@ -1,8 +1,8 @@
 import { PUBLIC_API_URL } from "$env/static/public";
 import { fetcher } from "$lib/utils/fetcher";
 
-export default async (name, campaign) => {
+export default async (name, color, campaign) => {
   const _url = `${PUBLIC_API_URL}/labels`;
-  const body = { name, campaign };
+  const body = { name, color, campaign };
   return fetcher(_url, "POST", body);
 };
--- ui/src/routes/campaigns/[campaign]/label/Labels.svelte
+++ ui/src/routes/campaigns/[campaign]/label/Labels.svelte
@@ -1,20 +1,22 @@
 <script>
   import labels from "$stores/labels.svelte.js";
   import { page } from "$app/stores";
+  import "$styles/color_input.css"
 
-  $effect(() => {
+  $effect(async () => {
     labels.retrieve($page.params.campaign);
   });
-
+  let color = $state("#000000")
   let newLabel = $state("");
   let disabled = $state(false);
+  let lineOpacity = $state("0F"); // hexadecimal value
 
   const createLabel = async (e) => {
     e.preventDefault();
     disabled = true;
     if (newLabel) {
       try {
-        await labels.create(newLabel, $page.params.campaign);
+        await labels.create(newLabel, color, $page.params.campaign);
         newLabel = "";
       } catch (e) {
         alert(e.message);
@@ -30,15 +32,18 @@
 
 <section>
   <h1>Labels</h1>
-  <form onsubmit={createLabel}>
+  <form class="flex" onsubmit={createLabel}>
     <input
       type="text"
       bind:value={newLabel}
       placeholder="new label"
       required
-      class="input"
+      class="rounded-r-none input"
     />
-    <button type="submit" class="btn btn-primary" {disabled}> Create </button>
+    <button type="submit" class="h-auto rounded-none btn btn-primary" {disabled}> Create </button>
+    <div class="box-border w-24 flex border-[1px] border-slate-400 rounded-tr-lg rounded-br-lg">
+      <input bind:value={color} class="box-border h-auto p-0 m-0 rounded-tr-lg rounded-br-lg" type="color" name="" id="">
+    </div>
   </form>
   <div>
     {#if labels.data.length == 0}
@@ -47,13 +52,19 @@
       <div class="flex flex-col items-start">
         {#each labels.data as label}
           <span
-            class="w-full flex justify-between {labels.current == label.name
-              ? 'bg-slate-300'
+            style="background-color: {label.color ? label.color : ""}{lineOpacity};
+                  
+            "
+            class="w-full my-1 hover:text-blue-900 hover:font-semibold flex justify-between rounded-md {labels.current == label.name
+              ? 'text-blue-900 font-semibold'
               : ''}"
           >
+            <div
+            style="background-color: {label.color ? label.color : ""};"
+            class="w-12 h-auto rounded-l-md"></div>
             <button
               onclick={() => (labels.current = label.name)}
-              class="hover:bg-slate-300 w-full">{label.name}</button
+              class="w-full">{label.name}</button
             >
             <button class="btn btn-error" onclick={() => deleteLabel(label.id)}
               >Delete</button
