f47732170057e3fad30da8262d3fc0cf0c934141
Fernando Hernandez <ferndohm99emf@gmail.com>
2024-11-04 22:51:21 +0100

    added color picker on labels

--- scaneo/src/usecases/labels/create_label.py
+++ scaneo/src/usecases/labels/create_label.py
@@ -1,8 +1,8 @@
 from ...models import Label
 from ...repos import LabelsDBRepo
 
-def create_label(name, campaign):	
+def create_label(name, color, campaign):	
 	repo = LabelsDBRepo()
-	label = Label(id=repo.generate_id(), name=name, campaign_id=campaign, color="#000000")
+	label = Label(id=repo.generate_id(), name=name, campaign_id=campaign, color=color)
 	repo.create_label(label)
 	return label
