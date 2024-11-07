4b2241422b5b90eef4f8913ee299fecc008e371f
Fernando Hernandez <ferndohm99emf@gmail.com>
2024-11-07 20:29:45 +0100

    Added bounding boxes and zoom to images

--- scaneo/src/models/annotation.py
+++ scaneo/src/models/annotation.py
@@ -18,9 +18,9 @@ class Annotation(BaseModel):
             id=data[0],
             type=data[1],
             value=data[2],
-            bb=json.loads(data[3]) if data[3] else None,
-            image_id=data[4],
-            createdAt=datetime.fromisoformat(data[5]),
-            updatedAt=datetime.fromisoformat(data[6])
+            image_id=data[3],
+            createdAt=datetime.fromisoformat(data[4]),
+            updatedAt=datetime.fromisoformat(data[5]),
+            bb=json.loads(data[6]) if data[6] else None
         )
 
--- scaneo/src/repos/ImagesDBRepo.py
+++ scaneo/src/repos/ImagesDBRepo.py
@@ -1,4 +1,6 @@
 from .DBRepo import DBRepo
+import rasterio
+import json
 
 class ImagesDBRepo(DBRepo):
     def __init__(self):
@@ -8,13 +10,22 @@ class ImagesDBRepo(DBRepo):
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             path TEXT NOT NULL,
             campaign_id TEXT,
+            bbox TEXT,
             FOREIGN KEY (campaign_id) REFERENCES campaigns(id)
         )""")
         self.commit_and_close_db()
 
+    def get_bbox(self, image):
+            src = rasterio.open(image)
+            bounds = src.bounds
+            crs = src.crs
+            if crs != "EPSG:4326":
+                bounds = rasterio.warp.transform_bounds(crs, "EPSG:4326", *bounds)
+            return json.dumps(bounds)
+    
     def create_images(self, images, campaign_id):
         cursor = self.get_cursor()
-        cursor.executemany("INSERT INTO images (path, campaign_id) VALUES (?, ?)", [(image, campaign_id) for image in images])
+        cursor.executemany("INSERT INTO images (path, campaign_id, bbox) VALUES (?, ?, ?)", [(image, campaign_id, self.get_bbox(image)) for image in images])
         self.commit_and_close_db()
 
     def retrieve_images(self, campaign_id):
