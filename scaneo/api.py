a520bc38f0bcad6a6e61e990e274004ac17844e0
Juan Sensio <sensio.juan@gmail.com>
2024-12-12 14:09:18 +0100

    base url

--- scaneo/api.py
+++ scaneo/api.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 import os
 
@@ -31,23 +30,6 @@ app.mount(
     ),
     name="ui",
 )
-
-# multi page app fallback
-@app.exception_handler(404)
-async def custom_404_handler(request: Request, exc):
-    path = request.url.path.lstrip("/")
-    html_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "ui", f"{path}.html")
-    
-    if os.path.isfile(html_file):
-        return FileResponse(html_file)
-    
-    # Fallback to index.html for SPA routes
-    index_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "ui", "index.html")
-    if os.path.isfile(index_file):
-        return FileResponse(index_file)
-    
-    return FileResponse(os.path.join(os.path.dirname(os.path.realpath(__file__)), "ui", "index.html"))
-
 
 if __name__ == "__main__":
     app()
--- scaneo/ui/_app/immutable/entry/start.BE9FaWAK.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.BC6k_p4V.js";export{t as start};
