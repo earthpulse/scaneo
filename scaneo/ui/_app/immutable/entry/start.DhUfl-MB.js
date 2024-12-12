fe28372c748809575e6df8c94783fc93bf6d679f
Juan Sensio <sensio.juan@gmail.com>
2024-12-12 15:38:27 +0100

    base url

---
+++ scaneo/ui/_app/immutable/entry/start.DhUfl-MB.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.drx7ywP6.js";export{t as start};
--- ui/src/lib/campaigns/retrieve.js
+++ ui/src/lib/campaigns/retrieve.js
@@ -1,12 +1,13 @@
 import { PUBLIC_API_URL } from "$env/static/public";
 import { fetcher } from "$lib/utils/fetcher";
+import baseUrl from "$stores/baseUrl.svelte.js";
 
 export async function retrieveCampaigns() {
-  const url = `${PUBLIC_API_URL}/_campaigns`;
+  const url = `${baseUrl.url}/_campaigns`;
   return fetcher(url);
 }
 
 export async function retrieveOneCampaign(campaign) {
-  const url = `${PUBLIC_API_URL}/_campaigns/${campaign}`;
+  const url = `${baseUrl.url}/_campaigns/${campaign}`;
   return fetcher(url);
 }
