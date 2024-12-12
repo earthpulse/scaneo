0d42e5cd86dc8dc2c11c6c508aa9c813e514c3be
Juan Sensio <sensio.juan@gmail.com>
2024-12-12 15:36:34 +0100

    base url

--- ui/src/routes/+layout.js
+++ ui/src/routes/+layout.js
@@ -1,2 +1,2 @@
 export const prerender = true;
-export const trailingSlash = "always"; // will prerender pages as index.html with rout name in folder, required by FastAPI static files
+// export const trailingSlash = "always"; // will prerender pages as index.html with rout name in folder, required by FastAPI static files
