c39acdf7128a8ce6c16a75c7b6b3657b0672f806
Juan Sensio <sensio.juan@gmail.com>
2024-12-16 11:42:48 +0100

    update

---
+++ scaneo/ui/_app/immutable/chunks/index-client.BS7WnwP4.js
@@ -0,0 +1,1 @@
+import{i as o,H as e,u as c,j as u}from"./runtime.cJOh_ioU.js";function a(n){o===null&&e(),o.l!==null?i(o).m.push(n):c(()=>{const t=u(n);if(typeof t=="function")return t})}function s(n){o===null&&e(),a(()=>()=>u(n))}function i(n){var t=n.l;return t.u??(t.u={a:[],b:[],m:[]})}export{a,s as o};
