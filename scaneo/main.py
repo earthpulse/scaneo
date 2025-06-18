55506b51f7b67740a0d17698a2b0886307106927
juansensio <sensio.juan@gmail.com>
2025-06-18 15:00:29 +0200

    update

--- scaneo/main.py
+++ scaneo/main.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 app = typer.Typer()
 
-__version__ = "2025.06.18-5"
+__version__ = "2025.06.18-6"
 
 @app.command()
 def run(
--- scaneo/ui/_app/immutable/entry/start._sj_OrOm.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.DtWVvX2Y.js";export{t as start};
